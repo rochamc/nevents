c1ec1f8bb449e94b50419b1c3678760d81f95761
logonrm <logonrm@fiap.com.br>
2017-02-06 22:14:17 -0200

    Initial commit

---
+++ routes/eventos.js
@@ -0,0 +1,10 @@
+module.exports = function (app) {
+    var eventos = app.controllers.eventos;
+    app.get('/menu', eventos.menu);
+    app.get('/cadUsuario', eventos.cadastroUsuario);
+    app.get('/cadEvento', eventos.cadastroEvento);
+    app.get('/listaEventos', eventos.listaEventos);
+
+    app.post('/novoEvento', eventos.novoEvento); 
+
+};
